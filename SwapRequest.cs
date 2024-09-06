282a74c53ce1ee3435e3a01c7acd49bb17dff497
bulanmurela <bulanaputrim@gmail.com>
2024-09-06 11:55:33 +0700

    Adding Class SwapRequest & Transaction

--- SwapRequest.cs
+++ SwapRequest.cs
@@ -8,5 +8,53 @@ namespace BookSwap
 {
     internal class SwapRequest
     {
+        private string _requestDate;
+        private string _requestID;
+        private string _req;
+        private string _status;
+
+        public string RequestDate
+        {
+            get { return _requestDate; }
+            set { _requestDate = value; }
+        }
+
+        public string RequestID
+        {
+            get { return _requestID; }
+            set { _requestID = value; }
+        }
+
+        public string Request
+        {
+            get { return _req; }
+            set { _req = value; }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public bool createRequest(string req)
+        {
+            if (req == "Bertukar")
+            {
+                _req = req;
+                _requestDate = DateTime.Now.ToString("dd/MM/yyyy"); // tanggal dinamis
+                _requestID = Guid.NewGuid().ToString(); // unique req ID dinamis
+                _status = "Requested";
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public string updateStatus(string status)
+        {
+            _status = status;
+            return _status;
+        }
     }
 }
--- Transaction.cs
+++ Transaction.cs
@@ -8,5 +8,45 @@ namespace BookSwap
 {
     internal class Transaction
     {
+        private string _transDate;
+        private string _transID;
+        private string _transStatus;
+
+        public string TransactionDate
+        {
+            get { return _transDate; }
+            set { _transDate = value; }
+        }
+
+        public string TransactionID
+        {
+            get { return _transID; }
+            set { _transID = value; }
+        }
+
+        public string TransactionStatus
+        {
+            get { return _transStatus; }
+            set { _transStatus = value; }
+        }
+
+        public bool completeTransaction(string transID)
+        {
+            if (!string.IsNullOrEmpty(transID))
+            {
+                _transID = transID;
+                _transDate = DateTime.Now.ToString("dd/MM/yyyy"); // set tanggal sekarang (dinamis)
+                _transStatus = "Complete";
+                return true;
+            }
+            else
+            { return  false; }
+        }
+
+        public string updateTransactionStatus(string newStatus)
+        {
+            _transStatus = newStatus;
+            return _transStatus;
+        }
     }
 }
