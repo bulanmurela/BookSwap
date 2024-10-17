5ce9664c7eadd2dfd50b30db7fd439436e8d7a88
tsaniagaluh <tsaniagaluhbanggash@mail.ugm.ac.id>
2024-10-17 11:58:42 +0700

    Update App Models

---
+++ BookSwapApp/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace BookSwapApp
+{
+    internal class Book
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string Genre { get; private set; }
+        public string Condition { get; private set; }
+        public string AvailabilityStatus { get; private set; }
+
+        public Book(string title, string author, string genre, string condition, string availabilityStatus)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Condition = condition;
+            AvailabilityStatus = availabilityStatus;
+        }
+
+        public void UpdateAvailabilityStatus(string status)
+        {
+            AvailabilityStatus = status;
+        }
+
+        public string GetBookDetails()
+        {
+            return $"Title: {Title}, Author: {Author}, Genre: {Genre}, Condition: {Condition}, AvailabilityStatus: {AvailabilityStatus}";
+        }
+
+        public void SetCondition(string condition)
+        {
+            Condition = condition;
+        }
+    }
+}
---
+++ BookSwapApp/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace BookSwapApp
+{
+    internal class Review
+    {
+        private string ReviewDate { get; set; }
+        private string Comment { get; set; }
+        private int Rating { get; set; }
+
+        public Review(string comment, int rating)
+        {
+            ReviewDate = DateTime.Now.ToString("yyyy-MM-dd");
+            Comment = comment;
+            Rating = rating;
+        }
+    }
+}
