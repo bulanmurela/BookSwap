ed7b9593129b0b7d6f761e57b115a8a0bb924a06
bulanmurela <bulanaputrim@gmail.com>
2024-10-20 21:29:46 +0700

    Add Navigation Services

--- BookSwapApp/MainWindow.xaml.cs
+++ BookSwapApp/MainWindow.xaml.cs
@@ -1,40 +1,18 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using BookSwapApp.Services;
 using BookSwapApp.Views;
 
 namespace BookSwapApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private NavigationService _navigationService;
+
         public MainWindow()
         {
             InitializeComponent();
-            // halaman default saat aplikasi dibuka
-            // MainFrame.Navigate(new Views.HomePage());
-        }
-
-        // navigasi untuk berpindah halaman
-        public void NavigateToPage(Page page)
-        {
-            MainFrame.Navigate(page);
-        }
-
-        private void btnToHomePage_Click(object sender, RoutedEventArgs e)
-        {
-            // Navigasi ke halaman HomePage
-            MainFrame.Navigate(new Views.HomePage());
+            _navigationService = new NavigationService(MainFrame);
+            _navigationService.NavigateTo(typeof(HomePage), _navigationService);
         }
     }
-
 }
---
+++ BookSwapApp/Services/NavigationServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace BookSwapApp.Services
+{
+    public class NavigationService
+    {
+        private readonly Frame _frame;
+
+        public NavigationService(Frame frame)
+        {
+            _frame = frame;
+        }
+
+        public void NavigateTo(Type pageType)
+        {
+            _frame.Navigate(Activator.CreateInstance(pageType));
+        }
+
+        public void NavigateTo(Type pageType, params object[] parameters)
+        {
+            var page = Activator.CreateInstance(pageType, parameters) as Page;
+            _frame.Navigate(page);
+        }
+
+        public void GoBack()
+        {
+            if (_frame.CanGoBack)
+            {
+                _frame.GoBack();
+            }
+        }
+    }
+}
