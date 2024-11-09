dcc7af5f657edaec4a0927628cfa835c27ae265d
tsaniagaluh <tsaniagaluhbanggash@mail.ugm.ac.id>
2024-11-09 13:44:20 +0700

    Add Upload Book Logic

--- BookSwapApp/MainWindow.xaml.cs
+++ BookSwapApp/MainWindow.xaml.cs
@@ -3,17 +3,20 @@ using BookSwapApp.Helpers;
 using BookSwapApp.Services;
 using BookSwapApp.Views;
 using BookSwapApp.Helpers;
+using BookSwapApp.Models;
 
 namespace BookSwapApp
 {
     public partial class MainWindow : Window
     {
         private NavigationService _navigationService;
+        private User _currentUser;
 
         public MainWindow()
         {
             InitializeComponent();
             _navigationService = new NavigationService(MainFrame);
+            _currentUser = new User();
             _navigationService.NavigateTo(typeof(HomePage), _navigationService);
 
             var dbHelper = new DatabaseHelpers();
